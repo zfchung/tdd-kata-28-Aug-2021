071b10c60ad9e0062d33773246ff761093d86c0f
ZF Chung (Zack) <zfchung1@gmail.com>
2021-08-28 17:38:08 +0800

    extrracted valueList const from numberValue const

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export function add(input: string): number {
   const stringValue = input ? input : "0";
-  const numberValue = stringValue.split(",").map(Number);
+  const valueList = stringValue.split(",");
+  const numberValue = valueList.map(Number);
   return numberValue[0] + (numberValue[1] ? numberValue[1] : 0);
 }
