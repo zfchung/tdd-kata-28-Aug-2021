eaa5d5026ee76d51c34ee89f1101ceb3608e7148
ZF Chung (Zack) <zfchung1@gmail.com>
2021-08-28 17:24:34 +0800

    add() returns sum of one number

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -4,4 +4,9 @@ describe("Test add function", () => {
   it("should return 0 for empty string", () => {
     expect(add("")).toEqual(0);
   });
+
+  it("should return the sum of one number", () => {
+    expect(add("1")).toEqual(1);
+    expect(add("99")).toEqual(99);
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export function add(input: string): number {
+  if (input) {
+    return parseInt(input);
+  }
   return 0;
 }
