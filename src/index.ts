b6625eb40e465dce0857c740673dc42313297835
ZF Chung (Zack) <zfchung1@gmail.com>
2021-08-28 17:40:21 +0800

    extracted sumValue const

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@ export function add(input: string): number {
   const stringValue = input ? input : "0";
   const valueList = stringValue.split(",");
   const numberValueList = valueList.map(Number);
-  return numberValueList[0] + (numberValueList[1] ? numberValueList[1] : 0);
+  const sumValue =
+    numberValueList[0] + (numberValueList[1] ? numberValueList[1] : 0);
+  return sumValue;
 }
