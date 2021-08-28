5ab1a86286dc2b673f21e4c51a720b5ea2a51d22
ZF Chung (Zack) <zfchung1@gmail.com>
2021-08-28 17:51:46 +0800

    extracted standardDelimiterString const from valueList const

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export function add(input: string): number {
   const stringValue = input ? input : "0";
-  const valueList = stringValue.replaceAll("\n", ",").split(",");
+  const standardDelimiterString = stringValue.replaceAll("\n", ",");
+  const valueList = standardDelimiterString.split(",");
   const numberValueList = valueList.map(Number);
   const sumValue = sumListItems(numberValueList);
   return sumValue;
